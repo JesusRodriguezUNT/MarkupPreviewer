52d8eb495f443c70162984cdce7186dddea22054
JesusRodriguezUNT <32721119+JesusRodriguezUNT@users.noreply.github.com>
2019-10-15 11:53:19 -0500

    Updates

---
+++ precache-manifest.74e60beabd1b2a225cab780a7e5434fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5b4f9775ff66f8be884d3e7ea11de5e",
+    "url": "/MarkupPreviewer/index.html"
+  },
+  {
+    "revision": "e392e98de6d81eb5e3f1",
+    "url": "/MarkupPreviewer/static/css/main.6124c52b.chunk.css"
+  },
+  {
+    "revision": "4ab7b6626e29fafa8712",
+    "url": "/MarkupPreviewer/static/js/2.46c37e53.chunk.js"
+  },
+  {
+    "revision": "e392e98de6d81eb5e3f1",
+    "url": "/MarkupPreviewer/static/js/main.661dc6bf.chunk.js"
+  },
+  {
+    "revision": "7c3c92f1bcc388343112",
+    "url": "/MarkupPreviewer/static/js/runtime-main.f02dd13f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/MarkupPreviewer/static/media/logo.25bf045c.svg"
+  }
+]);
